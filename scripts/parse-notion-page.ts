fd3c4b2de5d1dba4bbd0a8ab532e37579cb4ce76
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-02-02 03:03:00 +0900

    feat: update parse notion page

--- scripts/parse-notion-page.ts
+++ scripts/parse-notion-page.ts
@@ -9,6 +9,8 @@ export type NotionPage = {
     title: string
     slug: string
     createdAt: string
+    tags: string[]
+    description: string
 }
 
 /** Parse Notion page properties. */
@@ -21,7 +23,10 @@ export const parseProperties = (
         const title = titleFromPageObjectResponse(page)
         const slug = slugFromPageObjectResponse(page)
         const createdAt = createdAtFromPageObjectResponse(page)
-        return { pageId, title, slug, createdAt }
+        const tags = tagsFromPageObjectResponse(page)
+        const description = descriptionFromPageObjectResponse(page)
+        validateNotionPage({ title, slug, createdAt, tags, description })
+        return { pageId, title, slug, createdAt, tags, description }
     })
 
 /** Parse Notion page title. */
@@ -35,7 +40,7 @@ const titleFromPageObjectResponse = (
     return property.title[0].plain_text
 }
 
-/** Parse Notion page title. */
+/** Parse Notion page slug. */
 const slugFromPageObjectResponse = (
     pageObjectResponse: PageObjectResponse
 ): string => {
@@ -46,6 +51,7 @@ const slugFromPageObjectResponse = (
     return property.rich_text[0].plain_text
 }
 
+/** Parse Notion page createdAt. */
 const createdAtFromPageObjectResponse = (
     pageObjectResponse: PageObjectResponse
 ): string => {
@@ -56,5 +62,52 @@ const createdAtFromPageObjectResponse = (
     return property.date.start
 }
 
+/** Parse Notion page tags. */
+const tagsFromPageObjectResponse = (
+    pageObjectResponse: PageObjectResponse
+): string[] => {
+    const property = pageObjectResponse.properties.tags as {
+        type: 'multi_select'
+        multi_select: { id: string; name: string; color: string }[]
+    }
+    return property.multi_select.map((tag) => tag.name)
+}
+
+/** Parse Notion page description. */
+const descriptionFromPageObjectResponse = (
+    pageObjectResponse: PageObjectResponse
+): string => {
+    const property = pageObjectResponse.properties.description as {
+        type: 'rich_text'
+        rich_text: { plain_text: string }[]
+    }
+    return property.rich_text[0].plain_text
+}
+
 /**  */
-const validateNotionPage = ({ title: string }: { title: string }) => {}
+const validateNotionPage = ({
+    title,
+    slug,
+    createdAt,
+    tags,
+    description
+}: {
+    title: string
+    slug: string
+    createdAt: string
+    tags: string[]
+    description: string
+}) => {
+    if (title === '') {
+        throw Error('🚨 title is required.')
+    }
+    if (slug === '') {
+        throw Error('🚨 slug is required.')
+    }
+    if (createdAt === '') {
+        throw Error('🚨 createdAt is required.')
+    }
+    if (description === '') {
+        throw Error('🚨 description is required.')
+    }
+}
