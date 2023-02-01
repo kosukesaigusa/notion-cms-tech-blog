15fb3ca19ae874cdf287ebd55bf473a705cbdb2c
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-02-02 03:10:09 +0900

    feat: use fs/promises and update asynchronously write files process

--- scripts/export_markdowns.ts
+++ scripts/export_markdowns.ts
@@ -1,4 +1,5 @@
-import fs from 'fs'
+import console from 'console'
+import fs from 'fs/promises'
 
 import { NotionToMarkdown } from 'notion-to-md'
 
@@ -6,7 +7,7 @@ import { notionClient, queryDatabase } from './notion-api'
 import { NotionPage, parseProperties } from './parse-notion-page'
 
 /** Path to exported markdown files will be saved. */
-const EXPORTED_POSTS_RELATIVE_PATH = 'exported_posts'
+const EXPORTED_POSTS_RELATIVE_PATH = 'exported-posts'
 
 /** Notion to markdown package client. */
 const n2m = new NotionToMarkdown({ notionClient: notionClient })
@@ -17,7 +18,7 @@ const exportMarkdownsFromNotionDatabase = async () => {
     const posts = parseProperties(queryDatabaseResponse)
     console.log(`posts count: ${posts.length}`)
     for (const post of posts) {
-        writeMarkdownFile(post)
+        await writeMarkdownFile(post)
     }
 }
 
@@ -29,19 +30,17 @@ const writeMarkdownFile = async (notionPage: NotionPage) => {
     console.log(`💬 exporting post: ${title} (${pageId})`)
     const mdBlocks = await n2m.pageToMarkdown(pageId)
     const mdString = `# ${title}\n${n2m.toMarkdownString(mdBlocks)}`
-    fs.writeFile(
-        `${EXPORTED_POSTS_RELATIVE_PATH}/${slug}.md`,
-        mdString,
-        (err) => {
-            if (err === null) {
-                console.info(`🙌 Successfully exported: ${title} (${pageId})`)
-            } else {
-                console.error(
-                    `🚨 An error occurred when exporting page: ${title} (${pageId}): ${err}`
-                )
-            }
-        }
-    )
+    try {
+        await fs.writeFile(
+            `${EXPORTED_POSTS_RELATIVE_PATH}/${slug}.md`,
+            mdString
+        )
+        console.info(`🙌 Successfully exported: ${title} (${pageId})`)
+    } catch (e) {
+        console.error(
+            `🚨 An error occurred when exporting page: ${title} (${pageId}): ${e}`
+        )
+    }
 }
 
 /** Execute task. */
