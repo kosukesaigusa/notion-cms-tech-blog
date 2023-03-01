65723809387e843c167d95aaebb9a23fd502bf0d
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2023-03-01 10:12:30 +0900

    chore: remove unnecessary tag

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -8,14 +8,12 @@ import HeaderNavigation from './HeaderNavigation'
 /** Gives site layout. */
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <Flex minH={'100vh'} flexDirection={'column'}>
-        <HeaderNavigation />
-        <Box flexGrow={1} mb={20}>
-          <main>{children}</main>
-        </Box>
-        <Footer />
-      </Flex>
-    </>
+    <Flex minH={'100vh'} flexDirection={'column'}>
+      <HeaderNavigation />
+      <Box flexGrow={1} mb={20}>
+        <main>{children}</main>
+      </Box>
+      <Footer />
+    </Flex>
   )
 }
